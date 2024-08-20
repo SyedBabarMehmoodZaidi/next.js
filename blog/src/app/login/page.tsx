3dcdfc611348405eff7052e8cbaaed374fa5ab97
Babar Mehmood <babarzaidi1122@gmail.com>
2024-08-21 01:49:06 +0500

    Update page.tsx

--- blog/src/app/login/page.tsx
+++ blog/src/app/login/page.tsx
@@ -1,28 +1,29 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 
-const login =()=>{
-    const router = useRouter();
-    useEffect(() => {
-        if (!router) {
-            console.error("NextRouter is not mounted.");
-        }
-    }, [router]);
-    return(
-        <div>
-            <h1>LOGIN PAGE</h1>
-            <Link href="/">Go To Home Page</Link>
-            <br />
-            <br />
-            <button onClick={()=>router.push("/login/loginstudent")}>Go To Student Login Page</button>
-            <br />
-            <br />
-            <button>Go To Teacher Login Page</button>
-           
-        </div>
-    )
-}
+const Login = () => {
+  const router = useRouter();
+  const navigate = (page) => {
+    router.push("/login/" + page);
+  };
 
-export default login;
+  return (
+    <div>
+      <h1>LOGIN PAGE</h1>
+      <Link href="/">Go To Home Page</Link>
+      <br />
+      <br />
+      <button onClick={() => navigate("loginstudent")}>
+        Go To Student Login Page
+      </button>
+      <br />
+      <br />
+      <button onClick={() => navigate("loginteacher")}>
+        Go To Teacher Login Page
+      </button>
+    </div>
+  );
+};
+
+export default Login;
